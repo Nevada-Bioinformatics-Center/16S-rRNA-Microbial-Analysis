39d1d5e2610e3355da8b4310a3f73cb618126dc4
cassandra-hui <cassandrakay15@gmail.com>
2025-05-15 13:23:30 -0700

    Removing old Docker

--- 16S-rRNA-Docker/scripts/install_packages.R
+++
@@ -1,9 +0,0 @@
-# Contents of /16S-rRNA-Docker/16S-rRNA-Docker/scripts/install_packages.R
-
-# This script installs the R packages listed in r-requirements.txt
-
-# Read the package names from the requirements file
-packages <- readLines("r-requirements.txt")
-
-# Install the packages
-install.packages(packages, repos = "http://cran.r-project.org")
